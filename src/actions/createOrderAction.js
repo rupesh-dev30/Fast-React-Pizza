e96213e90102e6eff0c2a21af60ee0570917f529
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-22 01:22:23 +0530

    Data Loading complete

--- src/actions/createOrderAction.js
+++ src/actions/createOrderAction.js
@@ -1,6 +1,12 @@
 import { redirect } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
 
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
 export default async function action({ request }) {
   const formData = await request.formData();
 
@@ -12,6 +18,15 @@ export default async function action({ request }) {
     priority: data.priority === "on",
   };
 
+  const errors = {};
+
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  //if everything is okay, create new Order
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,11 +1,4 @@
-import { useState } from "react";
-import { Form } from "react-router-dom";
-
-// https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+import { Form, useActionData, useNavigation } from "react-router-dom";
 
 const fakeCart = [
   {
@@ -32,6 +25,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -53,6 +51,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -75,7 +74,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? "Placing Order..." : "Order now"}</button>
         </div>
       </Form>
     </div>
