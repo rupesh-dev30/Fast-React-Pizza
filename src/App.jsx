f01a9dfc9ba1a6c2ba4a4ab05afa877dbc6529ba
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-21 22:28:23 +0530

    newWay of routing 6+

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 export default function App() {
-  return (
-    <div>
-      Hlo Vite!
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
