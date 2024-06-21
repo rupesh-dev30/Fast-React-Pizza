cb5fa9c674d380914f10276fcfe13c641f316f52
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-21 23:43:54 +0530

    loading state while fetching

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+
+export default function AppLayout() {
+  const navigation = useNavigation();
+  // console.log(navigation);
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main>
+        <h1>Content</h1>
+      </main>
+
+      <CartOverview />
+      <Outlet />
+    </div>
+  )
+}
